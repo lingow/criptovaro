2ca281667ef23187ef416822612349778abd1527
Marco Caballero <caballeromarco@gmail.com>
2014-04-14 13:49:49 -0500

    1st commit from jdeveloper, just a test.

---
+++ Test.java
@@ -0,0 +1,11 @@
+/* Just a test source file for pushing into the git repo */
+
+public class Test {
+    public Test() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+    }
+}
