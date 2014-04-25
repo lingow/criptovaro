0ae0f175ab77f0fe314cb8da6e35c3cd4a831b3d
eduardo <eduardo@JLINGOW-LAP.st-users.us.oracle.com>
2014-04-24 23:47:09 -0500

    Added serializable version UID

--- criptovaro/Message.java
+++ criptovaro/Message.java
@@ -3,6 +3,8 @@ package criptovaro;
 import java.io.Serializable;
 
 public class Message implements Serializable{
+    @SuppressWarnings("compatibility:-5917982315562627774")
+    private static final long serialVersionUID = 1L;
     private MessageType type;
     private Serializable data;
 
