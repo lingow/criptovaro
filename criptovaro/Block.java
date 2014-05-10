40666983a364bce80b220471389e5452e7db0355
Marco Caballero <caballeromarco@gmail.com>
2014-05-10 00:48:51 -0500

    marcocg: Added following features: *Account - Account creation, key generation and accessors. *Transaction - Transaction creation, Sign, Verify and Accessors. *Miner - Added unit test in main for transaction and accounts *Block - Moved code from Transaction to generate inputs and outputs for a transaction. Commented out as it needs refactoring.

--- criptovaro/Block.java
+++ criptovaro/Block.java
@@ -1,5 +1,9 @@
 package criptovaro;
 
+import java.math.BigDecimal;
+
+import java.util.Arrays;
+
 public class Block {
     private Transaction[] transactions;
     private long proof;
@@ -9,8 +13,35 @@ public class Block {
         return null;
     }
 
-    public void addTransaction(Transaction tran) {
+    public void addTransaction(Transaction tran)
+    {
+        //Validate transaction signature
+        //Validate transaction semantics
+        //Add transaction inputs
+        //Add transaciton outputs
     }
+
+//    private boolean GenerateOutputs()
+//    {
+//        BigDecimal inputsAmount = BigDecimal.valueOf(0);
+//        
+//        //Are the inputs enough?
+//        for(Transaction t: inputs)
+//        {
+//            //Validate transaction semantics
+//            if(Arrays.equals(t.getDestination(), source))
+//                inputsAmount = inputsAmount.add(t.getAmount());
+//        }
+//        if(inputsAmount.compareTo(this.amount) < 0)
+//            return false; //Not enough funds. TODO: Log this event.
+//        
+//        //Generate change transaction 
+//        BigDecimal changeAmount = inputsAmount.subtract(amount);
+//        //We generate a change transaction with source and destiantion inverted for the change amount.
+//        outputs.add(new Transaction(destination, source, changeAmount));
+//        
+//        return true;
+//    }
 
     public byte[] getHash() {
         return null;
